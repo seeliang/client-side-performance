f22f2b8b2622c60edf382c894fb01973ec0005b5
seeliang <me@sihuiliang.com>
2020-02-27 08:01:23 +1100

    fix: start url issue

--- pwa/assets/service-worker.js
+++ pwa/assets/service-worker.js
@@ -1,4 +1,37 @@
 const CACHE_NAME = 'offline-cache-v1.3'
 const FILES_TO_CACHE = [
-  'static/offline.html',
+  '/',
+  '/static/index.html',
+  '/static/main.css',
 ]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page')
+      console.log(FILES_TO_CACHE);
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keyList => Promise.all(keyList.map((key) => {
+      if (key !== CACHE_NAME) {
+        console.log('[ServiceWorker] Removing old cache', key)
+        return caches.delete(key)
+      }
+    })))
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => caches.open(CACHE_NAME)
+          .then(cache => cache.match('offline.html')))
+    )
+  }
+})
