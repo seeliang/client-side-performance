f484f695a02cd40f2d633fc1f55cf7977bf924ed
seeliang <me@sihuiliang.com>
2020-04-05 22:46:55 +1000

    feat: add fallback and main

--- angular-io/src/app/app-routing.module.ts
+++ angular-io/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NotFoundComponent} from './not-found/not-found.component'
+import {MainComponent} from './main/main.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+    { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
