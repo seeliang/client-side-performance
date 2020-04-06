59a43beade899d7c344c01466732ad6c414ab260
seeliang <me@sihuiliang.com>
2020-04-06 10:40:17 +1000

    fix: remove double declear

--- angular-io/src/app/app.module.ts
+++ angular-io/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+
   ],
   imports: [
     BrowserModule,
