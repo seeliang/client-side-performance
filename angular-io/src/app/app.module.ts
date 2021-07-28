3e12fc7fe62c2c6d6bf3ccc50dcd3cbafd3343dc
seeliang <hi@sihuiliang.com>
2021-07-28 10:09:55 +1000

    add component in module

--- angular-io/src/app/app.module.ts
+++ angular-io/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component'
+import { HitComponent } from './hit/hit.component'
+import { MainComponent } from './main/main.component'
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
+    HitComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
